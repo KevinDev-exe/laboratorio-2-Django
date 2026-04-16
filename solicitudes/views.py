447ed0c077462397dede5cb7aa5b418246340a7f
DaniaU <daniauo2204@gmail.com>
2026-04-16 11:42:21 -0500

    vistas creadas

--- solicitudes/views.py
+++ solicitudes/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SolicitudForm
 
-# Create your views here.
+
+def registro_solicitud(request):
+    if request.method == 'POST':
+        form = SolicitudForm(request.POST, request.FILES)
+        if form.is_valid():
+            solicitud = form.save()
+            return redirect('solicitudes:confirmacion', pk=solicitud.pk)
+    else:
+        form = SolicitudForm()
+    return render(request, 'solicitudes/registro.html', {'form': form})
+
+
+def confirmacion_solicitud(request, pk):
+    from .models import Solicitud
+    solicitud = Solicitud.objects.get(pk=pk)
+    return render(request, 'solicitudes/confirmacion.html', {'solicitud': solicitud})
